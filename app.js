a1c56e73026329ffe4f248fb59e1014249d0d0a5
Plamen Mitev <pmitev89@gmail.com>
2019-06-04 16:43:30 +0200

    Use animate API

--- app.js
+++ app.js
@@ -1,31 +1,12 @@
-var canvas = document.getElementById('animated-canvas');
-var context = canvas.getContext('2d');
-var canvasInterval = requestAnimationFrame(canvasAnimation);
-var circleX = 60;
-var velocity = 1;
-var acceleration = 0.5;
-
-function canvasAnimation() {
-  circleX++;
-  drawCircle(circleX, canvas.height/2, 25);
-  canvasInterval = requestAnimationFrame(canvasAnimation);
-}
-
-function drawCircle(x, y, s) {
-  clearCanvas();
-  context.fillStyle = "red";
-  context.beginPath();
-  context.arc(x, y, s, 0, 2 * Math.PI);
-  context.fill();
-  velocity += acceleration;
-  circleX += velocity;
-  if(circleX > canvas.width) {
-    circleX = 0;
-    velocity = 1;
+var rotatingSquare = document.getElementById("web-animation").animate(
+  [
+    {backgroundColor: '#000000', opacity: 1.0, transform: 'rotate(0deg) translate3D(0, 0, 0)' },
+    {backgroundColor: '#444000', opacity: 0.3 },
+    {backgroundColor: '#000000', opacity: 1.0, transform: 'rotate(360deg) translate3D(0, 0, 0)' }
+  ],
+  {
+    duration: 500,
+    iterations: Infinity,
+    easing: 'ease-in-out'
   }
-}
-
-function clearCanvas() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-}
-drawCircle();
+)
